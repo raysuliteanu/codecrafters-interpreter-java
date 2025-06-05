a2d90d957839f7f079101281a0ac2354b7a8597f
Ray Suliteanu <raysuliteanu@gmail.com>
2025-06-05 14:26:14 -0700

    start parse command support

--- src/main/java/lox/Main.java
+++ src/main/java/lox/Main.java
@@ -8,14 +8,15 @@ import java.util.List;
 public class Main {
   public static void main(String[] args) {
     if (args.length < 2) {
-      System.err.println("Usage: ./your_program.sh tokenize <filename>");
+      System.err.println("Usage: ./your_program.sh <command> <filename>");
+      System.err.println("Commands: tokenize, parse, evaluate");
       System.exit(1);
     }
 
     String command = args[0];
     String filename = args[1];
 
-    if (!command.equals("tokenize")) {
+    if (!command.equals("tokenize") && !command.equals("parse") && !command.equals("evaluate")) {
       System.err.println("Unknown command: " + command);
       System.exit(1);
     }
@@ -30,23 +31,35 @@ public class Main {
 
     int rc = 0;
     if (fileContents.length() > 0) {
-      Result<List<Token>, List<Throwable>> result = new Scanner().scan(fileContents);
+      switch (command) {
+        case "tokenize":
+          Result<List<Token>, List<Throwable>> result = new Scanner().scan(fileContents);
 
-      if (result.hasErr()) {
-        rc = 65;
-        for (var error : result.error()) {
-          System.err.println(error);
-        }
-      }
+          if (result.hasErr()) {
+            rc = 65;
+            for (var error : result.error()) {
+              System.err.println(error);
+            }
+          }
 
-      if (result.isOk()) {
-        for (var token : result.success()) {
-          System.out.println(token);
-        }
+          if (result.isOk()) {
+            for (var token : result.success()) {
+              System.out.println(token);
+            }
+          }
+          System.out.println("EOF  null");
+          break;
+        case "parse":
+          System.err.println("Parse command not yet implemented");
+          rc = 1;
+          break;
+        case "evaluate":
+          System.err.println("Evaluate command not yet implemented");
+          rc = 1;
+          break;
       }
     }
 
-    System.out.println("EOF  null");
     System.exit(rc);
   }
 }
