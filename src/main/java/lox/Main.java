3338e98deb1935c88e076e475a5094c99d676b46
Ray Suliteanu <raysuliteanu@gmail.com>
2025-06-12 13:41:12 -0700

    codecrafters submit [skip ci]

--- src/main/java/lox/Main.java
+++ src/main/java/lox/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 import lox.parse.Ast;
 import lox.token.Scanner;
@@ -11,74 +12,97 @@ import lox.token.Token;
 
 public class Main {
 
-  public static void main(String[] args) {
-    if (args.length < 2) {
-      System.err.println("Usage: ./your_program.sh <command> <filename>");
-      System.err.println("Commands: tokenize, parse, evaluate");
-      System.exit(1);
+    enum Options {
+        tokenize,
+        parse,
+        evaluate,
     }
 
-    String command = args[0];
-    String filename = args[1];
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: ./your_program.sh <command> <filename>");
+            System.err.println("Commands: tokenize, parse, evaluate");
+            System.exit(1);
+        }
 
-    if (!command.equals("tokenize") &&
-        !command.equals("parse") &&
-        !command.equals("evaluate")) {
-      System.err.println("Unknown command: " + command);
-      System.exit(1);
+        Options command = null;
+        try {
+            command = Options.valueOf(args[0]);
+        } catch (Exception e) {
+            System.err.println("Unknown command: " + args[0]);
+            System.exit(1);
+        }
+
+        String filename = args[1];
+
+        int rc = 0;
+        Optional<String> fileContents;
+        switch (command) {
+            case tokenize:
+                fileContents = readFile(filename);
+                if (fileContents.isPresent()) {
+                    Result<List<Token>, List<Throwable>> result = new Scanner().scan(fileContents.get());
+
+                    if (result.hasErr()) {
+                        rc = 65;
+                        for (var error : result.error()) {
+                            System.err.println(error);
+                        }
+                    }
+
+                    if (result.isOk()) {
+                        for (var token : result.success()) {
+                            System.out.println(token);
+                        }
+                    }
+                }
+                System.out.println("EOF  null");
+                break;
+            case parse:
+                fileContents = readFile(filename);
+                if (fileContents.isPresent()) {
+                    Result<List<Ast>, List<Throwable>> parse = new lox.parse.Parser(fileContents.get()).parse();
+                    if (parse.hasErr()) {
+                        rc = 65;
+                        for (var error : parse.error()) {
+                            System.err.println(error);
+                        }
+                    }
+
+                    if (parse.isOk()) {
+                        for (var ast : parse.success()) {
+                            // System.out.println("(" + ast + ")");
+                            System.out.println(ast);
+                        }
+                    }
+                }
+                break;
+            case evaluate:
+                System.err.println("Evaluate command not yet implemented");
+                rc = 1;
+                break;
+        }
+
+        System.exit(rc);
     }
 
-    String fileContents = "";
-    try {
-      fileContents = Files.readString(Path.of(filename));
-    } catch (IOException e) {
-      System.err.println("Error reading file: " + e.getMessage());
-      System.exit(1);
+    /**
+     * Read file contents to a String.
+     *
+     * Read the file contents and return the data as a String. If the file is empty,
+     * or there is an error reading the file, the Optional will be empty.
+     */
+    private static Optional<String> readFile(String filename) {
+        String fileContents = null;
+        try {
+            Path path = Path.of(filename);
+            if (Files.size(path) > 0) {
+                fileContents = Files.readString(path);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+
+        return Optional.ofNullable(fileContents);
     }
-
-    int rc = 0;
-    if (fileContents.length() > 0) {
-      switch (command) {
-        case "tokenize":
-          Result<List<Token>, List<Throwable>> result = new Scanner()
-              .scan(fileContents);
-
-          if (result.hasErr()) {
-            rc = 65;
-            for (var error : result.error()) {
-              System.err.println(error);
-            }
-          }
-
-          if (result.isOk()) {
-            for (var token : result.success()) {
-              System.out.println(token);
-            }
-          }
-          System.out.println("EOF  null");
-          break;
-        case "parse":
-          Result<List<Ast>, List<Throwable>> parse = new lox.parse.Parser(fileContents).parse();
-          if (parse.hasErr()) {
-            rc = 65;
-            for (var error : parse.error()) {
-              System.err.println(error);
-            }
-          }
-
-          if (parse.isOk()) {
-            for (var ast : parse.success()) {
-              System.out.println("(" + ast + ")");
-            }
-          }
-          break;
-        case "evaluate":
-          System.err.println("Evaluate command not yet implemented");
-          rc = 1;
-          break;
-      }
-    }
-
-    System.exit(rc);
-  }
 }
--- src/main/java/lox/parse/Parser.java
+++ src/main/java/lox/parse/Parser.java
@@ -1,5 +1,7 @@
 package lox.parse;
 
+import static lox.util.LogUtil.trace;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,251 +13,254 @@ import lox.token.Scanner;
 import lox.token.Token;
 import lox.token.Tokens.Lexemes;
 import lox.util.IterablePeekableIterator;
-import lox.util.LogUtil;
 import lox.util.PeekableIterator;
 import lox.util.UnexpectedEofException;
 
 public class Parser {
 
-  private final Scanner scanner;
-  private final CharSequence source;
+    private final Scanner scanner;
+    private final CharSequence source;
 
-  public Parser(final CharSequence source) {
-    this.scanner = new Scanner();
-    this.source = source;
-  }
-
-  public Result<List<Ast>, List<Throwable>> parse() {
-    final var scanResult = scanner.scan(source);
-
-    // TODO: handle possible errors from scan()
-
-    final var ast = new ArrayList<Ast>();
-    final var errors = new ArrayList<Throwable>();
-
-    if (scanResult.isOk()) {
-      final var tokens = scanResult.success();
-
-      final var it = new IterablePeekableIterator<Token>(tokens);
-
-      return program(it, errors);
+    public Parser(final CharSequence source) {
+        this.scanner = new Scanner();
+        this.source = source;
     }
 
-    return new Result<>(ast, errors);
-  }
+    public Result<List<Ast>, List<Throwable>> parse() {
+        final var scanResult = scanner.scan(source);
 
-  private Result<List<Ast>, List<Throwable>> program(final PeekableIterator<Token> tokens, List<Throwable> errors) {
-    final var nodes = new ArrayList<Ast>();
-    while (tokens.hasNext()) {
-      try {
-        final var token = tokens.peek().get();
-        LogUtil.trace("next token: " + token);
-        final var ast = switch (token.lexeme()) {
-          case CLASS -> throw new NotImplementedException(token.lexeme().name());
-          case FUN -> throw new NotImplementedException(token.lexeme().name());
-          case VAR -> throw new NotImplementedException(token.lexeme().name());
-          default -> statement(tokens);
+        final var ast = new ArrayList<Ast>();
+        final var errors = new ArrayList<Throwable>();
+
+        if (scanResult.isOk()) {
+            final var tokens = scanResult.success();
+
+            final var it = new IterablePeekableIterator<Token>(tokens);
+
+            return program(it, errors);
+        }
+
+        return new Result<>(ast, errors);
+    }
+
+    private Result<List<Ast>, List<Throwable>> program(final PeekableIterator<Token> tokens, List<Throwable> errors) {
+        final var nodes = new ArrayList<Ast>();
+        while (tokens.hasNext()) {
+            try {
+                final var token = tokens.peek().get();
+                trace("next token: " + token);
+                final var ast = switch (token.lexeme()) {
+                    case CLASS -> throw new NotImplementedException(token.lexeme().name());
+                    case FUN -> throw new NotImplementedException(token.lexeme().name());
+                    case VAR -> throw new NotImplementedException(token.lexeme().name());
+                    default -> statement(tokens);
+                };
+                nodes.add(ast);
+            } catch (LoxException e) {
+                errors.add(e);
+            }
+        }
+
+        return new Result<>(nodes, errors);
+    }
+
+    private Ast statement(final PeekableIterator<Token> tokens) {
+        trace("stmt");
+        final var ast = switch (tokens.peek().get().lexeme()) {
+            case LEFT_BRACE -> block(tokens);
+            case WHILE -> whileStmt(tokens);
+            case RETURN -> returnStmt(tokens);
+            case FOR -> forStmt(tokens);
+            case IF -> ifStmt(tokens);
+            case PRINT -> printStmt(tokens);
+            default -> expressionStatement(tokens);
         };
-        nodes.add(ast);
-      } catch (LoxException e) {
-        errors.add(e);
-      }
+
+        return ast;
     }
 
-    return new Result<>(nodes, errors);
-  }
+    private Ast expressionStatement(final PeekableIterator<Token> tokens) {
+        trace("expression stmt");
+        final var ast = expression(tokens);
 
-  private Ast statement(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("stmt");
-    final var ast = switch (tokens.peek().get().lexeme()) {
-      case LEFT_BRACE -> block(tokens);
-      case WHILE -> whileStmt(tokens);
-      case RETURN -> returnStmt(tokens);
-      case FOR -> forStmt(tokens);
-      case IF -> ifStmt(tokens);
-      case PRINT -> printStmt(tokens);
-      default -> expressionStatement(tokens);
-    };
+        // check for an eat semicolon, or else error
+        // TODO: for now, comment out checking for semicolon
+        // since we're not at the statement stage in CC
+        /*
+         * tokens.nextIf((t) -> t.lexeme() == Lexemes.SEMICOLON)
+         * .orElseThrow(() -> {
+         * if (tokens.hasNext()) {
+         * return new MissingTokenException(
+         * Lexemes.SEMICOLON.value(),
+         * tokens.peek().get().lexeme().value());
+         * } else {
+         * return new UnexpectedEofException();
+         * }
+         * });
+         */
 
-    return ast;
-  }
-
-  private Ast expressionStatement(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("expression stmt");
-    final var ast = expression(tokens);
-
-    // check for an eat semicolon, or else error
-    tokens.nextIf((t) -> t.lexeme() == Lexemes.SEMICOLON)
-        .orElseThrow(() -> {
-          if (tokens.hasNext()) {
-            return new MissingTokenException(
-                Lexemes.SEMICOLON.value(),
-                tokens.peek().get().lexeme().value());
-          } else {
-            return new UnexpectedEofException();
-          }
-        });
-
-    return ast;
-  }
-
-  private Expr expression(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("expression");
-    return equality(tokens);
-  }
-
-  private Expr equality(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("equality");
-    var ast = comparison(tokens);
-
-    while (tokens.hasNext()) {
-      final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.BANG_EQUAL || t.lexeme() == Lexemes.EQUAL_EQUAL);
-      if (node.isPresent()) {
-        final var op = node.get();
-        final var rhs = comparison(tokens);
-        ast = new Expr.Binary(op, ast, rhs);
-      } else {
-        break;
-      }
+        return ast;
     }
 
-    return ast;
-  }
-
-  private Expr comparison(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("comparison");
-    var ast = term(tokens);
-
-    while (tokens.hasNext()) {
-      final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.GREATER ||
-          t.lexeme() == Lexemes.GREATER_EQUAL ||
-          t.lexeme() == Lexemes.LESS_EQUAL ||
-          t.lexeme() == Lexemes.LESS);
-      if (node.isPresent()) {
-        final var op = node.get();
-        final var rhs = term(tokens);
-        ast = new Expr.Binary(op, ast, rhs);
-      } else {
-        break;
-      }
-    }
-    return ast;
-  }
-
-  private Expr term(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("term");
-
-    var ast = factor(tokens);
-
-    while (tokens.hasNext()) {
-      final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.PLUS || t.lexeme() == Lexemes.MINUS);
-
-      if (node.isPresent()) {
-        final var op = node.get();
-        final var rhs = factor(tokens);
-        ast = new Expr.Binary(op, ast, rhs);
-      } else {
-        break;
-      }
+    private Expr expression(final PeekableIterator<Token> tokens) {
+        trace("expression");
+        return equality(tokens);
     }
 
-    return ast;
-  }
+    private Expr equality(final PeekableIterator<Token> tokens) {
+        trace("equality");
+        var ast = comparison(tokens);
 
-  private Expr factor(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("factor");
+        while (tokens.hasNext()) {
+            final var node = tokens
+                    .nextIf((t) -> t.lexeme() == Lexemes.BANG_EQUAL || t.lexeme() == Lexemes.EQUAL_EQUAL);
+            if (node.isPresent()) {
+                final var op = node.get();
+                final var rhs = comparison(tokens);
+                ast = new Expr.Binary(op, ast, rhs);
+            } else {
+                break;
+            }
+        }
 
-    var ast = unary(tokens);
-
-    while (tokens.hasNext()) {
-      final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.STAR || t.lexeme() == Lexemes.SLASH);
-
-      if (node.isPresent()) {
-        final var op = node.get();
-        final var rhs = unary(tokens);
-        ast = new Expr.Binary(op, ast, rhs);
-      } else {
-        break;
-      }
+        return ast;
     }
 
-    return ast;
-  }
+    private Expr comparison(final PeekableIterator<Token> tokens) {
+        trace("comparison");
+        var ast = term(tokens);
 
-  private Expr unary(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("unary");
-    var e = switch (tokens.nextIf((t) -> t.lexeme() == Lexemes.BANG || t.lexeme() == Lexemes.MINUS)) {
-      case Optional<Token> o when o.isPresent() -> {
-        final var op = o.get();
-        final Expr expr = primary(tokens);
-        yield new Expr.Unary(op, expr);
-      }
-      default -> {
-        var p = primary(tokens);
-        yield p;
-      }
-    };
-
-    return e;
-  }
-
-  private Expr primary(final PeekableIterator<Token> tokens) {
-    LogUtil.trace("primary");
-    var token = tokens
-        .nextIf((t) -> t.lexeme() == Lexemes.TRUE || t.lexeme() == Lexemes.FALSE || t.lexeme() == Lexemes.NIL);
-
-    if (token.isPresent()) {
-      return new Expr.Terminal(token.get());
+        while (tokens.hasNext()) {
+            final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.GREATER ||
+                    t.lexeme() == Lexemes.GREATER_EQUAL ||
+                    t.lexeme() == Lexemes.LESS_EQUAL ||
+                    t.lexeme() == Lexemes.LESS);
+            if (node.isPresent()) {
+                final var op = node.get();
+                final var rhs = term(tokens);
+                ast = new Expr.Binary(op, ast, rhs);
+            } else {
+                break;
+            }
+        }
+        return ast;
     }
 
-    token = tokens.nextIf((t) -> t.lexeme() == Lexemes.LEFT_PAREN);
-    if (token.isPresent()) {
-      // start of group expression i.e. '(' expr ')'
-      LogUtil.trace("start group expr");
-      var expr = expression(tokens);
-      token = tokens.nextIf((t) -> t.lexeme() == Lexemes.RIGHT_PAREN);
-      if (token.isPresent()) {
-        LogUtil.trace("end group expr");
-        return new Expr.Group(expr);
-      } else if (tokens.hasNext()) {
-        throw new MissingTokenException(")", tokens.peek().get().lexeme().value());
-      } else {
-        throw new UnexpectedEofException();
-      }
+    private Expr term(final PeekableIterator<Token> tokens) {
+        trace("term");
+
+        var ast = factor(tokens);
+
+        while (tokens.hasNext()) {
+            final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.PLUS || t.lexeme() == Lexemes.MINUS);
+
+            if (node.isPresent()) {
+                final var op = node.get();
+                final var rhs = factor(tokens);
+                ast = new Expr.Binary(op, ast, rhs);
+            } else {
+                break;
+            }
+        }
+
+        return ast;
     }
 
-    token = tokens.nextIf(
-        (t) -> t.lexeme() == Lexemes.NUMBER || t.lexeme() == Lexemes.STRING || t.lexeme() == Lexemes.IDENTIFIER);
-    if (token.isPresent()) {
-      return new Expr.Terminal(token.get());
+    private Expr factor(final PeekableIterator<Token> tokens) {
+        trace("factor");
+
+        var ast = unary(tokens);
+
+        while (tokens.hasNext()) {
+            final var node = tokens.nextIf((t) -> t.lexeme() == Lexemes.STAR || t.lexeme() == Lexemes.SLASH);
+
+            if (node.isPresent()) {
+                final var op = node.get();
+                final var rhs = unary(tokens);
+                ast = new Expr.Binary(op, ast, rhs);
+            } else {
+                break;
+            }
+        }
+
+        return ast;
     }
 
-    // TODO: make exception type
-    throw new RuntimeException("unexpected token '" + tokens.peek().get().lexeme().value() + "'");
-  }
+    private Expr unary(final PeekableIterator<Token> tokens) {
+        trace("unary");
+        var e = switch (tokens.nextIf((t) -> t.lexeme() == Lexemes.BANG || t.lexeme() == Lexemes.MINUS)) {
+            case Optional<Token> o when o.isPresent() -> {
+                final var op = o.get();
+                final Expr expr = primary(tokens);
+                yield new Expr.Unary(op, expr);
+            }
+            default -> {
+                var p = primary(tokens);
+                yield p;
+            }
+        };
 
-  private Ast block(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("block statement");
-  }
+        return e;
+    }
 
-  private Ast whileStmt(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("while statement");
-  }
+    private Expr primary(final PeekableIterator<Token> tokens) {
+        trace("primary");
+        var token = tokens
+                .nextIf((t) -> t.lexeme() == Lexemes.TRUE || t.lexeme() == Lexemes.FALSE || t.lexeme() == Lexemes.NIL);
 
-  private Ast returnStmt(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("return statement");
-  }
+        if (token.isPresent()) {
+            return new Expr.Terminal(token.get());
+        }
 
-  private Ast forStmt(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("for statement");
-  }
+        token = tokens.nextIf((t) -> t.lexeme() == Lexemes.LEFT_PAREN);
+        if (token.isPresent()) {
+            // start of group expression i.e. '(' expr ')'
+            trace("start group expr");
+            var expr = expression(tokens);
+            token = tokens.nextIf((t) -> t.lexeme() == Lexemes.RIGHT_PAREN);
+            if (token.isPresent()) {
+                trace("end group expr");
+                return new Expr.Group(expr);
+            } else if (tokens.hasNext()) {
+                throw new MissingTokenException(")", tokens.peek().get().lexeme().value());
+            } else {
+                throw new UnexpectedEofException();
+            }
+        }
 
-  private Ast ifStmt(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("if statement");
-  }
+        token = tokens.nextIf(
+                (t) -> t.lexeme() == Lexemes.NUMBER || t.lexeme() == Lexemes.STRING
+                        || t.lexeme() == Lexemes.IDENTIFIER);
+        if (token.isPresent()) {
+            return new Expr.Terminal(token.get());
+        }
 
-  private Ast printStmt(final PeekableIterator<Token> tokens) {
-    throw new NotImplementedException("print statement");
-  }
+        // TODO: make exception type
+        throw new RuntimeException("unexpected token '" + tokens.peek().get().lexeme().value() + "'");
+    }
+
+    private Ast block(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("block statement");
+    }
+
+    private Ast whileStmt(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("while statement");
+    }
+
+    private Ast returnStmt(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("return statement");
+    }
+
+    private Ast forStmt(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("for statement");
+    }
+
+    private Ast ifStmt(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("if statement");
+    }
+
+    private Ast printStmt(final PeekableIterator<Token> tokens) {
+        throw new NotImplementedException("print statement");
+    }
 
 }
