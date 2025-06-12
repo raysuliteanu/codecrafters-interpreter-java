62082a961087f8563a9f36508bd18cae39d72ced
Ray Suliteanu <raysuliteanu@gmail.com>
2025-06-12 13:44:26 -0700

    codecrafters submit [skip ci]

--- src/main/java/lox/parse/AstPrinter.java
+++ src/main/java/lox/parse/AstPrinter.java
@@ -13,91 +13,91 @@ import lox.parse.Stmt.WhileStmt;
 
 public class AstPrinter implements AstVisitor<String> {
 
-  public String print(Ast ast) {
-    return ast.accept(this);
-  }
-
-  @Override
-  public String visitTerminal(Expr.Terminal expr) {
-    if (expr.token() instanceof lox.token.ValueToken vt) {
-      return vt.value().toString();
-    } else {
-      return expr.token().lexeme().value();
+    public String print(Ast ast) {
+        return ast.accept(this);
     }
-  }
 
-  @Override
-  public String visitGroup(Expr.Group expr) {
-    return "(" + expr.group() + ")";
-  }
+    @Override
+    public String visitTerminal(Expr.Terminal expr) {
+        if (expr.token() instanceof lox.token.ValueToken vt) {
+            return vt.value().toString();
+        } else {
+            return expr.token().lexeme().value();
+        }
+    }
 
-  @Override
-  public String visitUnary(Expr.Unary expr) {
-    return expr.token().lexeme().value() + expr.expr();
-  }
+    @Override
+    public String visitGroup(Expr.Group expr) {
+        return "(group " + expr.group() + ")";
+    }
 
-  @Override
-  public String visitBinary(Expr.Binary expr) {
-    return expr.op().lexeme().value() + " " + expr.left() + " " + expr.right();
-  }
+    @Override
+    public String visitUnary(Expr.Unary expr) {
+        return expr.token().lexeme().value() + expr.expr();
+    }
 
-  @Override
-  public String visitExprStmt(ExprStmt exprStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitExprStmt'");
-  }
+    @Override
+    public String visitBinary(Expr.Binary expr) {
+        return expr.op().lexeme().value() + " " + expr.left() + " " + expr.right();
+    }
 
-  @Override
-  public String visitPrintStmt(PrintStmt printStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitPrintStmt'");
-  }
+    @Override
+    public String visitExprStmt(ExprStmt exprStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitExprStmt'");
+    }
 
-  @Override
-  public String visitReturnStmt(ReturnStmt returnStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitReturnStmt'");
-  }
+    @Override
+    public String visitPrintStmt(PrintStmt printStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitPrintStmt'");
+    }
 
-  @Override
-  public String visitWhileStmt(WhileStmt whileStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitWhileStmt'");
-  }
+    @Override
+    public String visitReturnStmt(ReturnStmt returnStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitReturnStmt'");
+    }
 
-  @Override
-  public String visitForStmt(ForStmt forStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitForStmt'");
-  }
+    @Override
+    public String visitWhileStmt(WhileStmt whileStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitWhileStmt'");
+    }
 
-  @Override
-  public String visitIfStmt(IfStmt ifStmt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitIfStmt'");
-  }
+    @Override
+    public String visitForStmt(ForStmt forStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitForStmt'");
+    }
 
-  @Override
-  public String visitClazz(Clazz clazz) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitClazz'");
-  }
+    @Override
+    public String visitIfStmt(IfStmt ifStmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitIfStmt'");
+    }
 
-  @Override
-  public String visitFunc(Func func) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitFunc'");
-  }
+    @Override
+    public String visitClazz(Clazz clazz) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitClazz'");
+    }
 
-  @Override
-  public String visitVar(Var var) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitVar'");
-  }
+    @Override
+    public String visitFunc(Func func) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitFunc'");
+    }
 
-  @Override
-  public String visitBlock(Block block) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'visitBlock'");
-  }
+    @Override
+    public String visitVar(Var var) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitVar'");
+    }
+
+    @Override
+    public String visitBlock(Block block) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitBlock'");
+    }
 }
